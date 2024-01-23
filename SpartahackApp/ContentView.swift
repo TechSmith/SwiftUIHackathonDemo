79b076255203c3d78e78ea4589345f1bac1e223f
Carson Greene <c.greene@techsmith.com>
2024-01-23 16:26:36 -0500

    Show a placeholder visit logger from the main view

--- SpartahackApp/ContentView.swift
+++ SpartahackApp/ContentView.swift
@@ -9,48 +9,50 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+   @Environment(\.modelContext) private var modelContext
+   @Query private var items: [Item]
+   
+   @State private var visitLoggerShowing = false
 
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+   var body: some View {
+      NavigationSplitView {
+         List {
+            ForEach(items) { item in
+               NavigationLink {
+                  Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+               } label: {
+                  Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+               }
             }
-            .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-            .toolbar {
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
+            .onDelete(perform: deleteItems)
+         }
+         .navigationSplitViewColumnWidth(min: 180, ideal: 200)
+         .toolbar {
+            ToolbarItem {
+               Button(action: logVisit) {
+                  Label("Add Item", systemImage: "plus")
+                  }
+               }
+         }
+      } detail: {
+         Text("Select an item")
+      }
+      .sheet(isPresented: $visitLoggerShowing) {
+         
+      }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
+   private func logVisit() {
+      visitLoggerShowing = true
+   }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+   private func deleteItems(offsets: IndexSet) {
+      withAnimation {
+         for index in offsets {
+            modelContext.delete(items[index])
+         }
+      }
+   }
 }
 
 #Preview {
---
+++ SpartahackApp/VisitLoggerView.swift
@@ -0,0 +1,18 @@
+//
+//  VisitLoggerView.swift
+//  SpartahackApp
+//
+//  Created by Carson Greene on 1/23/24.
+//
+
+import SwiftUI
+
+struct VisitLoggerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VisitLoggerView()
+}
