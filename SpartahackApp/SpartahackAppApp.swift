ef40b759adedb8b0c881bae8dbf15e16f6abb264
Carson Greene <c.greene@techsmith.com>
2024-01-25 12:08:07 -0500

    Fix things after the rename

--- SpartahackApp/SpartahackAppApp.swift
+++ SpartahackApp/SpartahackAppApp.swift
@@ -25,7 +25,7 @@ struct SpartahackAppApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
         .modelContainer(sharedModelContainer)
     }
