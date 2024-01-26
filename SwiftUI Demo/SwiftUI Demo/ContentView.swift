ee394d932a3c42fce1117a870797bbe2abf21813
Carson Greene <c.greene@techsmith.com>
2024-01-26 10:16:29 -0500

    Add some examples to the sample app

--- SwiftUI Demo/SwiftUI Demo/ContentView.swift
+++ SwiftUI Demo/SwiftUI Demo/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State var sheetShowing = false
+   @State var checkboxIsChecked = false
    
    var body: some View {
       VStack {
@@ -15,6 +17,27 @@ struct ContentView: View {
             .imageScale(.large)
             .foregroundStyle(.tint)
          Text("Hello, world!")
+            //.foregroundStyle(.red)
+            //.bold()
+            //.italic()
+         Button(action: { sheetShowing.toggle() }) {
+            Text("Show Sheet")
+         }
+         Text("Checkbox \(checkboxIsChecked ? "is" : "isn't") checked")
+      }
+      .padding(8)
+      .sheet(isPresented: $sheetShowing) {
+         CheckboxView(checkboxIsChecked: $checkboxIsChecked)
+      }
+   }
+}
+
+struct CheckboxView: View {
+   @Binding var checkboxIsChecked: Bool
+   
+   var body: some View {
+      Toggle(isOn: $checkboxIsChecked) {
+         Text("Is Checked?")
       }
       .padding()
    }
